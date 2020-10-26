bd26191bbaf74fc8164501110b3d8dee621a3d59
Rich Alossi <alossix@gmail.com>
2020-10-26 09:27:19 +0100

    Update game logic

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-// Sets up the environment
+// ENVIRONMENT
 const { Engine, Render, Runner, World, Bodies, Body } = Matter;
 const engine = Engine.create();
 engine.world.gravity.y = 0;
+Body.frictionStatic = 0;
 const { world } = engine;
 const render = Render.create({
   element: document.body,
@@ -13,89 +14,85 @@ const render = Render.create({
 });
 Render.run(render);
 Runner.run(Runner.create(), engine);
+let sizeW = render.options.width;
+let sizeH = render.options.height;
 
-// Left/Right Walls
+// WALLS
 const walls = [
-  Bodies.rectangle(
-    0,
-    render.options.height / 2,
-    50,
-    render.options.height * 2,
-    {
-      isStatic: true,
-    }
-  ),
-  Bodies.rectangle(
-    render.options.width,
-    render.options.height / 2,
-    50,
-    render.options.height * 2,
-    {
-      isStatic: true,
-    }
-  ),
+  Bodies.rectangle(0, sizeH / 2, 50, sizeW * 2, {
+    isStatic: true,
+  }),
+  Bodies.rectangle(sizeW, sizeH / 2, 50, sizeH * 2, {
+    isStatic: true,
+  }),
 ];
+walls.forEach((wall) => {
+  wall.density = 1000;
+});
 World.add(world, walls);
 
-// create new player and add to environment
-const playerBody = Bodies.rectangle(
-  render.options.width / 2,
-  render.options.height,
-  50,
-  200,
-  {
-    isStatic: false,
-  }
-);
-playerBody.friction = 0;
+// PLAYER: Create new player and add to environment
+const playerBody = Bodies.circle(sizeW / 2, sizeH - 80, sizeW / 80, {
+  isStatic: false,
+});
 playerBody.frictionAir = 0;
 World.add(world, playerBody);
 console.log(playerBody);
 
-// Player movement and firing
-let ammo = [];
+// PLAYER MOVEMENT
 document.addEventListener("keydown", (event) => {
   const { x, y } = playerBody.velocity;
   if (event.code === "ArrowLeft") {
     Body.setVelocity(playerBody, { x: x - 12, y: 0 });
+    Body.setPosition(playerBody, { x: playerBody.position.x, y: sizeH - 80 });
   } else if (event.code === "ArrowRight") {
     Body.setVelocity(playerBody, { x: x + 12, y: 0 });
+    Body.setPosition(playerBody, { x: playerBody.position.x, y: sizeH - 80 });
   } else if (event.code === "Space") {
-    ammo.unshift(
-      Bodies.circle(playerBody.position.x, playerBody.position.y - 110, 4, {
+    let ammo = Bodies.circle(
+      playerBody.position.x,
+      playerBody.position.y - 110,
+      4,
+      {
         isStatic: false,
-      })
+      }
     );
-    Body.setVelocity(ammo[0], { x: 0, y: -15 });
-    ammo[0].frictionAir = 0;
-    ammo[0].density = 1;
-    World.add(world, ammo[0]);
+    ammo.frictionAir = 0;
+    Body.setVelocity(ammo, { x: 0, y: -16 });
+    World.add(world, ammo);
   }
 });
 document.addEventListener("keyup", (event) => {
   if (event.code === "ArrowLeft" || event.code === "ArrowRight") {
     Body.setVelocity(playerBody, { x: 0, y: 0 });
-    Body.setAngle(playerBody, 0);
-    Body.setAngularVelocity(playerBody, 0);
   }
 });
 
-// create new enemies
-let interval = 1500;
+// DIFFICULTY LEVEL
+let interval;
+let difficulty = 1;
+if (difficult === 1) {
+  interval = 1500;
+  // change endgame at enemies.length = 75?
+}
+if (difficulty === 2) {
+  interval = 1000;
+  // change endgame at enemies.length = 150?
+} else if (difficulty === 3) {
+  interval = 800;
+  // change endgame at enemies.length = 250?
+}
+
+// CREATE ENEMIES
 const enemies = [];
 for (let i = 0; i < 100; i++) {
   enemies.push(
-    Bodies.circle(
-      Math.floor(Math.random() * render.options.width),
-      0,
-      render.options.width / 40,
-      {
-        isStatic: false,
-      }
-    )
+    Bodies.circle(Math.random() * ((sizeW / 10) * 9), 0, sizeW / 40, {
+      isStatic: false,
+    })
   );
 }
-// append enemies to world and launch from above
+// ADD ENEMIES TO WORLD AND LAUNCH ATTACK
 enemies.forEach((enemy, index) => {
   enemy.frictionAir = 0;
   setTimeout(() => {
@@ -103,9 +100,7 @@ enemies.forEach((enemy, index) => {
     Body.applyForce(
       enemy,
       { x: enemy.position.x, y: enemy.position.y },
-      { x: 0, y: 0.025 }
+      { x: 0, y: 0.02 }
     );
   }, index * interval);
 });
-
-// Player cannon
