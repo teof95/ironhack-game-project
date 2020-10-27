91a2a203a95a2271804aeb4b3f02685ee48bfe31
Rich Alossi <alossix@gmail.com>
2020-10-27 14:03:46 +0100

    Tweak movement mechanics

--- src/collision.js
+++ src/collision.js
@@ -7,7 +7,9 @@ const isOffScreen = function () {
     for (i = 3; i < world.bodies.length; i++) {
       if (world.bodies[i].position.y > sizeH) {
         World.remove(world, world.bodies[i]);
-        scoreCount -= 5;
+        if (scoreCount >= 0) {
+          scoreCount -= 5;
+        }
       } else if (world.bodies[i].position.y < -200) {
         World.remove(world, world.bodies[i]);
       }
@@ -20,15 +22,22 @@ isOffScreen();
 Events.on(engine, "collisionStart", ({ pairs }) => {
   pairs.forEach(({ bodyA, bodyB }) => {
     if (bodyA.id === 3 && bodyB.id > 3) {
+      playerBody.isStatic = true;
       World.remove(world, bodyB);
-      Body.setPosition(playerBody, { x: sizeW / 2, y: sizeH - 80 });
-      numLives--;
-      console.log(`scoreCount is ${scoreCount}`);
     } else if (bodyA.id > 3 && bodyB.id > 3) {
       World.remove(world, bodyA);
       World.remove(world, bodyB);
       scoreCount += 5;
-      console.log(`scoreCount is ${scoreCount}`);
+    }
+  });
+});
+Events.on(engine, "collisionEnd", ({ pairs }) => {
+  pairs.forEach(({ bodyA, bodyB }) => {
+    if (bodyA.id === 3 && bodyB.id > 3) {
+      Body.setPosition(playerBody, { x: sizeW / 2, y: sizeH - 80 });
+      Body.setVelocity(playerBody, { x: 0, y: 0 });
+      playerBody.isStatic = false;
+      numLives--;
     }
   });
 });
--- src/playercreate.js
+++ src/playercreate.js
@@ -1,5 +1,6 @@
 // PLAYER: Create new player and add to environment
 const playerBody = Bodies.circle(sizeW / 2, sizeH - 80, sizeW / 60);
+// console.log(playerBody);
 playerBody.frictionAir = 0;
 playerBody.friction = 0;
 World.add(world, playerBody);
@@ -7,7 +8,7 @@ World.add(world, playerBody);
 // PLAYER MOVEMENT
 let xMove = sizeW / 60;
 document.addEventListener("keydown", (event) => {
-  const { x, y } = playerBody.velocity;
+  let { x, y } = playerBody.velocity;
   if (event.code === "ArrowLeft") {
     Body.setVelocity(playerBody, { x: x - xMove, y: 0 });
   } else if (event.code === "ArrowRight") {
